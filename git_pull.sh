7bdbaf25461ea9027d47f9be9c5d53ae29e94358
Timor <liaojy@authine.com>
2021-01-29 10:26:29 +0800

    更新

--- git_pull.sh
+++ git_pull.sh
@@ -37,14 +37,14 @@ ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
 WhichDep=$(grep "/jd-base" "${ShellDir}/.git/config")
-Scripts2URL=https://github.com/shylocks/Loon
+Scripts2URL=https://github.com/kongbg/timorScript2
 
 if [[ ${WhichDep} == *github* ]]; then
-  ScriptsURL=https://github.com/LXK9301/jd_scripts
-  ShellURL=https://github.com/EvineDeng/jd-base
+  ScriptsURL=https://github.com/kongbg/timorScript
+  ShellURL=https://github.com/kongbg/jd-base
 else
-  ScriptsURL=https://gitee.com/lxk0301/jd_scripts
-  ShellURL=https://gitee.com/evine/jd-base
+  ScriptsURL=https://github.com/kongbg/timorScript
+  ShellURL=https://github.com/kongbg/jd-base
 fi
 
 ## 更新shell脚本
